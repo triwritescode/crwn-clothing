d14353fe89cd3d027d4bec2f1c24f93fe43acb6f
Tri Denda <triwritescode@gmail.com>
2025-12-05 14:38:17 +0700

    installing sass and adding directory and category component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./categories.styles.scss";
+import Directory from "./components/directory/directory.component";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "/images/hats.jpg",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "/images/jackets.jpg",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "/images/sneakers.jpg",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "/images/womens.jpg",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "/images/mens.jpg",
+    },
+  ];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  return <Directory categories={categories} />;
+};
 
-export default App
+export default App;
